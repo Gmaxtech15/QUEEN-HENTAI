a7b1ea24d4df3422880c52ab1060f959d09901ee
Gmaxtech15 <102092930+Gmaxtech15@users.noreply.github.com>
2023-11-15 01:43:01 +0300

    Update config.js

--- config.js
+++ config.js
@@ -47,11 +47,11 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = '💝 Queen Hentai 💝'
+global.botname = '💝 Gmax Bot 💝'
 global.premium = 'true'
-global.packname = '💝 Queen Hentai 💝 Created By 💝 Dinuwa Official²⁰²³ 💝' 
-global.author = '💝 Dinuwa Official²⁰²³ 💝' 
-global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/Dinuwa Official²⁰²³\n' 
+global.packname = '💝 Queen Hentai 💝 Created By 💝 GmaxtechTz Official²⁰²³ 💝' 
+global.author = '💝 GmaxtectTz Official²⁰²³ 💝' 
+global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/GMAXTECHtz Official²⁰²³\n' 
 global.dygp = 'https://chat.whatsapp.com/Jyjqx7KBfgjDC7QcefHB1S'
 global.fgsc = 'https://github.com/dinuwah/QUEEN-HENTAI' 
 global.fgyt = 'https://chat.whatsapp.com/Jyjqx7KBfgjDC7QcefHB1S'
